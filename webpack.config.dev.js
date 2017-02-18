257fed3907fe191e8626ab4f3128da865844ca9c
Nicholas Mascia <nmmascia@gmail.com>
2017-02-18 11:16:38 -0500

    Update dependencies

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,26 +5,24 @@ const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const cssValues = require('postcss-modules-values');
 
-const dir = path.resolve('.');
-const src = path.join(dir, 'src');
+const src = path.join(__dirname, 'src');
 
 module.exports = {
   devtool: 'sourcemap',
   entry: [
     'react-hot-loader/patch',
     'babel-polyfill',
-    'eventsource-polyfill',
     'webpack-hot-middleware/client',
     './src/index',
   ],
   output: {
-    path: path.join(dir, 'dist', 'dev'),
+    path: path.join(__dirname, 'dist', 'dev'),
     filename: 'bundle.js',
     publicPath: '/static/',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       process: {
         env: {
@@ -37,41 +35,50 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
+        loaders: [
+          'babel-loader',
+        ],
         include: src,
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[path][name]___[hash:base64:5]!postcss-loader!',
+        loaders: [
+          'style-loader',
+          'css-loader?modules',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => ([
+                autoprefixer,
+                precss,
+                cssValues,
+              ]),
+            },
+          },
+        ],
+        include: src,
       },
       {
         test: /\.(jpg|png|mp3|wav|ogg)$/,
-        loader: 'url-loader',
+        loaders: [
+          'url-loader',
+        ],
         include: src,
       },
       {
         test: /\.json$/,
-        loader: 'json5-loader',
+        loaders: [
+          'json5-loader',
+        ],
         include: src,
       },
     ],
-    postcss: () => {
-      return [
-        autoprefixer,
-        precss,
-        cssValues,
-      ];
-    },
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-    modulesDirectories: ['node_modules'],
-    root: src,
-  },
-  externals: {
-    cheerio: 'window',
-    'react/addons': true,
-    'react/lib/ExecutionEnvironment': true,
-    'react/lib/ReactContext': true,
+    modules: [
+      src,
+      'node_modules',
+    ],
+    extensions: ['.js', '.jsx'],
   },
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -8,11 +8,10 @@ const cssValues = require('postcss-modules-values');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const dir = path.resolve('.');
-const src = path.join(dir, 'src');
+const src = path.join(__dirname, 'src');
 
 module.exports = {
   devtool: 'sourcemap',
-  progress: true,
   entry: [
     'babel-polyfill',
     './src/index',
@@ -30,9 +29,8 @@ module.exports = {
         },
       },
     }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
       compress: {
         warnings: false,
       },
@@ -45,35 +43,50 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
+        loaders: [
+          'babel-loader',
+        ],
         include: src,
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[hash:base64:5]!postcss-loader!',
+        loaders: [
+          'style-loader',
+          'css-loader?modules',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => ([
+                autoprefixer,
+                precss,
+                cssValues,
+              ]),
+            },
+          },
+        ],
+        include: src,
       },
       {
         test: /\.(jpg|png|mp3|wav|ogg)$/,
-        loader: 'file-loader',
+        loaders: [
+          'url-loader',
+        ],
         include: src,
       },
       {
         test: /\.json$/,
-        loader: 'json5-loader',
+        loaders: [
+          'json5-loader',
+        ],
         include: src,
       },
     ],
-    postcss: () => {
-      return [
-        autoprefixer,
-        precss,
-        cssValues,
-      ];
-    },
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-    modulesDirectories: ['node_modules'],
-    root: src,
+    modules: [
+      src,
+      'node_modules',
+    ],
+    extensions: ['.js', '.jsx'],
   },
 };
